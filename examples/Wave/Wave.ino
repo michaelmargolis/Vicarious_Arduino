4d42acf047f6d4764abb05fb750ac754fc618fa7
michaelmargolis <memargolis@hotmail.com>
2015-02-01 22:11:14 +0000

    Updated examples

--- examples/Wave/Wave.ino
+++ examples/Wave/Wave.ino
@@ -2,35 +2,50 @@
 #include <Vicarious.h>
 #include <Servo.h>
 
-const dataId_t HASTINGS = 1; // the id for the desired data stream
-const int interval     = 100; // milliseconds between requests
-const int SERVO_PIN    = 5;   // Pin connected to servo
+const consumerId_t HASTINGS = 1;   // the id for the desired data stream
+const int consumerInterval  = 100; // milliseconds between requests
+const int smoothing         = 8;   // number of samples to smooth
 
-Vicarious wave;  //create an instance of a vicarious data stream
-Servo  myServo;   // create a servo instance
+VicariousConsumer wave;  //create an instance of a vicarious data stream
+
+const int SERVO_PIN = 5; // Pin connected to servo
+Servo  myServo;          // create a servo instance
 
 void setup()
 {
-  Serial.begin(9600); //debug messages are sent over USB
-  wave.begin(HASTINGS);
-  Serial.print("connecting ..");
-  while ( wave.status() != DATA_AVAILABLE) {
+  Serial.begin(9600);  //debug messages are sent over USB
+  vicarious.begin();   // start the API (link speed is set in library)
+  Serial.print("Connecting to Vicarious server");  
+  while ( vicarious.isConnected() == false) {
     // here until a connection is valid and data is available
     Serial.print(".");
     delay(100);
   }
-  wave.mapData(0, 255, 0, 180); // map all data to servo range
-  myServo.attach(SERVO_PIN);
-  Serial.println(" ready");
+  if(wave.begin(HASTINGS, BYTE_TYPE)) {   
+    Serial.print("Connecting to Hastings wave consumer");
+    while (wave.status() != DATA_AVAILABLE) {
+      // here until a connection is valid and data is available
+      Serial.print(".");
+      delay(100);
+    }
+    Serial.println(" ok");
+    wave.smoothData(smoothing);   
+    wave.setInterval(consumerInterval);
+    wave.mapData(0, 255, 0, 180); // map all data to servo range
+    myServo.attach(SERVO_PIN);
+    Serial.println(" ready");
+  }
 }
 
 void loop()
 {
-  if (wave.status() == DATA_AVAILABLE) {
-    int angle = wave.read();
-    myServo.write(angle);
-    delay(interval);
+  if( wave.isReadTime() ) {
+    if (wave.status() == DATA_AVAILABLE) {
+      int angle = wave.readByte();
+      myServo.write(angle);     
+    }
   }
 }
+
 
 
